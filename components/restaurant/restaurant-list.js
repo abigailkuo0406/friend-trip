0d2ced70d462c5bb2163eb50921091c6ebb3db27
Abigail <christinekuo905@gmail.com>
2023-07-07 16:39:48 +0800

    0707餐廳切版

--- components/restaurant/restaurant-list.js
+++ components/restaurant/restaurant-list.js
@@ -1,32 +1,37 @@
 import React from 'react'
-// import styles from './restaurant.css'
+import Image from 'next/image'
 
+import Img from '@/data/restaurant/rest-img.json'
+import restaurant from '@/data/restaurant/rest-detail.json'
 
 export default function RestaurantList() {
   return (
-    <div
-      classNameName="card"
-      style={{ width: 18 + 'rem' }}
-      d-flex
-      flex-row
-      mb-3
-    >
-      <img src="..." className="card-img-top" />
-      <div className="card-body">
-        <h5 className="card-title">長角96</h5>
-        <p className="card-text">
-          營業時間：週一~週日 11:30-22:00(供餐至21:00) 料理特色：牛排、酒吧
-          訂位須知：...
-        </p>
-        <div d-flex justify-content-evenly>
-          <a href="#" className="btn btn-primary">
-            邀請好友
-          </a>
-          <a href="#" className="btn btn-primary">
-            訂位
-          </a>
+    <>
+      <div className="card mb-3" style={{ 'max-width': 540 + 'px' }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <Image src={Img.RestImg} className="img-fluid rounded-start" alt="..."/>
+
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5
+                className="card-title"
+              >Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
