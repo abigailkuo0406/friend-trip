2272b42b804ad86c80b92538969e4da0e6e4196a
Abigail <christinekuo905@gmail.com>
2023-07-10 11:34:57 +0800

    餐廳列表map

--- components/restaurant/restaurant-list.js
+++ components/restaurant/restaurant-list.js
@@ -1,39 +1,28 @@
 import React from 'react'
 import Image from 'next/image'
-
-import Img from '@/data/restaurant/rest-img.json'
-
-import restaurant from '@/data/restaurant/rest-detail.json'
-
+import A from '@/assets/rest-img/rest-img-1-c.jpg'
 
 export default function RestaurantList(props) {
-  // console.log(props)
+  console.log(props.img)
 
   return (
     <>
       <div className="card mb-3" style={{ 'max-width': 540 + 'px' }}>
         <div className="row g-0">
           <div className="col-md-4">
-            <Image src={Img.RestImg} className="img-fluid rounded-start" alt="..." />
-
+            <Image
+              src={A}
+              className="img-fluid rounded-start"
+              alt="..."
+              // width={200 + 'px'}
+            />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              {restaurant.map((rest, id) => {
-                return
-                <h5
-                  className="card-title" key={id}
-                >
-                  {rest.title}
-                </h5>
-              })}
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">{props.details}</p>
               <p className="card-text">
-                {props.details}
-              </p>
-              <p className="card-text">
-                <small className="text-body-secondary">
-                  Last updated 3 mins ago
-                </small>
+                <button>訂位</button>
               </p>
             </div>
           </div>
--- pages/restaurant/index.js
+++ pages/restaurant/index.js
@@ -7,17 +7,21 @@ import RestaurantList from '@/components/restaurant/restaurant-list'
 // 引入假資料
 import restaurant from '@/data/restaurant/rest-detail.json'
 
-
-
 export default function Rest() {
- 
   return (
     <>
       <h1>精選餐廳</h1>
-      <RestaurantList details="aa" />
-      
-
-
+      {restaurant.map((v, i) => {
+        return (
+          <div key={v.RestID}>
+            <RestaurantList
+              img={v.RestImg}
+              title={v.RestName}
+              details={v.RestIntro}
+            />
+          </div>
+        )
+      })}
     </>
   )
 }
