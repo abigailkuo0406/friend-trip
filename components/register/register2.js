7ca1994b1e62a82b41c812223a9f9b002febc7a9
John <japanwork6@gmail.com>
2023-07-11 18:09:14 +0800

    07/11

---
+++ components/register/register2.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import styles from './register2.module.css'
+export default function RegisterCard() {
+  return (
+    <>
+        <div className={styles.main}>
+            <div className={styles.cardbody}>
+                <div>
+                    <h2>深入資料</h2>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel} className={styles.inputlabel}>身高</h5>
+                    <input type='text' className={styles.input} ></input>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>體重</h5>
+                    <input type='text' className={styles.input}></input>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>星座</h5>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>血型</h5>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>抽菸</h5>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">有</label>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">沒有</label>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>酒精</h5>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">滴酒不沾</label>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">小酌</label>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">酒豪</label>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>教育程度</h5>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">男</label>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">女</label>
+                </div>
+                <div className={styles.inputstyle}>
+                    <h5 className={styles.inputlabel}>工作</h5>
+                    <input type='text' className={styles.input}></input>
+                </div>
+                <div className={styles.inputstyle}>
+                <label for="w3review" className={styles.inputlabel}>自我介紹</label>
+                <textarea id="w3review" name="w3review" rows="4" cols="50">
+                自我介紹轎討美例平爸的美今記與。王進將采五處是常一主城度斜有許誓我聞人懼沾至攸站…；中別光摯現觀樹角討撒此聲法！穩在士致猿到受禾陳方柴弟陳度上報傾實體嘶死至過…或望白梓場了了知們妞間跟，一西民式速盎水在對龍進
+                </textarea></div>
+            </div>
+        </div>
+    </>
+  )
+}
---
+++ pages/register/page2.js
@@ -0,0 +1,9 @@
+import React from "react"
+import RegisterLetter from "@/components/register/register2"
+export default function Register(){
+    return(
+        <>
+            <RegisterLetter />
+        </>
+    )
+}
