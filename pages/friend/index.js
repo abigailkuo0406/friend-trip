8b5a33b4201544d6a1d24d7ad56a77b55dc902b3
John <japanwork6@gmail.com>
2023-07-12 16:44:17 +0800

    0712

--- pages/friend/index.js
+++ pages/friend/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Friend from '@/components/friend/friend'
+import AdminLayout from '@/components/layout/admin-layout'
 
 export default function Friendhome() {
   return (
@@ -7,4 +8,10 @@ export default function Friendhome() {
       <Friend />
     </>
   )
+}
+
+// 這裡代表要套用AdminLayout，取代原本的DefaultLayout
+// 要寫在元件的函式之外
+Friendhome.getLayout = function (page) {
+  return <AdminLayout>{page}</AdminLayout>
 }
