aa384fff0cd9af0173ab5cacf1a489349b6d8e30
Abigail <christinekuo905@gmail.com>
2023-07-14 13:40:19 +0800

    後端連接測試

--- pages/test.js
+++ pages/test.js
@@ -1,39 +1,136 @@
-export default function Test() {
-  return (
-    <div className="d-flex justify-content-center align-items-center">
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        按我來測試跳出視窗(modal)
-      </button>
+import { useEffect, useState, useContext } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                跳出視窗(modal)標題
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">...</div>
+export default function Products() {
+  const router = useRouter()
+  console.log(router)
+  const [data, setData] = useState({
+    redirect: '',
+    totalRows: 0,
+    perPage: 4,
+    totalPages: 0,
+    page: 1,
+    rows: [],
+  })
+  const [test, settest] = useState({
+    name: "hellooooooooooooo",
+    age: 998989898989898989898,
+  })
+  console.log('router.query:', router.query)
+  const [keyword, setKeyword] = useState('')
+
+  useEffect(() => {
+    setKeyword(router.query.keyword || '')
+    const usp = new URLSearchParams(router.query)
+    console.log('USP:', usp + ' typ:', typeof usp)
+    fetch(`${process.env.API_SERVER}/test?${usp.toString()}`, {
+      method: "POST",
+      body:JSON.stringify(test),
+
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, [router.query])
+
+  return (
+    <>
+      <Head>
+        <title>abcd</title>
+      </Head>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <nav aria-label="Page navigation example">
+              <ul className="pagination">
+                {Array(5)
+                  .fill(1)
+                  .map((v, i) => {
+                    const p = data.page - 2 + i
+                    const query = { ...router.query }
+                    if (p < 1 || p > data.totalPages) return
+                    query.page = p
+                    return (
+                      <li
+                        className={
+                          `page-item ` + (p === data.page ? 'active' : '')
+                        }
+                        key={p}
+                      >
+                        <Link
+                          className="page-link"
+                          href={'?' + new URLSearchParams(query).toString()}
+                        >
+                          {p}
+                        </Link>
+                      </li>
+                    )
+                  })}
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <form
+              className="d-flex"
+              role="search"
+              onSubmit={(e) => {
+                e.preventDefault()
+                router.push(`?keyword=` + e.currentTarget.keyword.value)
+              }}
+            >
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                name="keyword"
+                value={keyword}
+                onChange={(e) => {
+                  setKeyword(e.currentTarget.value)
+                }}
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <table className="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>書名</th>
+                  <th>作者</th>
+                  <th>價格</th>
+                  <th>書號</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.rows.map((i) => (
+                  <tr key={i.sid}>
+                    <td>{i.sid}</td>
+                    <td>
+                      <Link href={'/products/' + i.sid}> {i.bookname} </Link>
+                    </td>
+                    <td>{i.author}</td>
+                    <td>{i.price}</td>
+                    <td>{i.book_id}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
