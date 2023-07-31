f588634a19e2f38fcaae4a841ccab50860efb8c9
John <japanwork6@gmail.com>
2023-07-31 09:11:58 +0800

    0730

--- pages/member/account.js
+++ pages/member/account.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import Friend from '@/components/friend/friend2'
 import AdminLayout from '@/components/layout/admin-layout'
-
-export default function Friendhome2() {
+import Edit from '@/components/edit/edit'
+export default function EditHome() {
   return (
     <>
       <div className="d-flex justify-content-center">
-        <Friend />
+        <Edit />
       </div>
     </>
   )
 }
-
 // 這裡代表要套用AdminLayout，取代原本的DefaultLayout
 // 要寫在元件的函式之外
-Friendhome2.getLayout = function (page) {
+EditHome.getLayout = function (page) {
   return <AdminLayout>{page}</AdminLayout>
 }
