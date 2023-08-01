4a3abf5e84d479bf24e8762e2c48b36c84f2828d
abigailkuo <abigailkuo0406@gmail.com>
2023-08-01 13:43:25 +0800

    修正input-date元件最大值最小值bug

--- components/common/input/input-date.js
+++ components/common/input/input-date.js
@@ -14,25 +14,24 @@ export default function InputDate({
   addClassforInput = '',
   addClassforLabel = '',
 }) {
- 
+
   // 定義預設日期
   const today = new Date()
   const nowYear = today.getFullYear()
-  const nowMonth = today.getMonth() + 1
-  const nextMonth = nowMonth + 1
-  const nowDate = today.getDate()
+  const nowMonth = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1
+  const nextMonth = today.getMonth() + 2 < 10 ? `0${today.getMonth() + 2}` : today.getMonth() + 2
+  const nowDate = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate()
 
   // 定義起始日期為當日
-  const nowDay =
-    nowMonth < 10
-      ? `${nowYear}-0${nowMonth}-${nowDate}`
-      : `${nowYear}-${nowMonth}-${nowDate}`
+  const nowDay = `${nowYear}-${nowMonth}-${nowDate}`
+
 
   // 定義結束日期為一個月後
   const maxDay =
     nextMonth > 12
       ? `${nowYear + 1}-01-${nowDate}`
-      : `${nowYear}-0${nextMonth}-${nowDate}`
+      : `${nowYear}-${nextMonth}-${nowDate}`
+
 
   const [inputValue1, setInputValue1] = useState(value)
   // useEffect(() => {
@@ -40,7 +39,7 @@ export default function InputDate({
   //   getName(name)
   // }, []) // 在第一次渲染時，如有預設 value 將 value 和 name 先傳回去，避免預設值會讀不到
 
-  
+
   const handleChange = (e) => {
     setInputValue1(e.target.value)
     // getValue(e.target.value)
@@ -63,9 +62,9 @@ export default function InputDate({
         className={`${width} input-text`}
         onChange={handleChange}
       />
-      
+
     </div>
-    
-    
+
+
   )
 }
