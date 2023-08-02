aaac82d108e023245dd457d0aef7e216a6607ac6
Thomas <niutingyuan@gmail.com>
2023-08-01 18:22:56 -0800

    留言板圖片改存在posts的img裡面了

--- components/forum-comps/Posts.jsx
+++ components/forum-comps/Posts.jsx
@@ -10,15 +10,17 @@ export default function Posts({ posts }) {
             key={post.post_id}
             id={post.post_id}
             avatarOfPostAuthor={
-              post.file_url ||
-              'https://images.unsplash.com/photo-1501443762994-82bd5dace89a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+              post.img
+              // post.file_url ||
+              // 'https://images.unsplash.com/photo-1501443762994-82bd5dace89a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
             }
             authorOfThePost={post.username}
             isLogginUserAuthorOfThePost={post.isLogginUserAuthorOfThePost}
             dateOfPublishOfPost={post.created_at}
             imgOfPost={
-              post.file_url ||
-              'https://images.unsplash.com/photo-1501443762994-82bd5dace89a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+              post.img
+              // post.file_url ||
+              // 'https://images.unsplash.com/photo-1501443762994-82bd5dace89a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
             }
             // imgOfPost={post.img}
             articleOfPost={post.content}
