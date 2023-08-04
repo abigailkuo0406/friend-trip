1e3978fb3147a8b0c1a1f4a8181b627944349717
John <japanwork6@gmail.com>
2023-08-04 20:04:15 +0800

    test

--- components/register/imgupload.js
+++ components/register/imgupload.js
@@ -31,7 +31,7 @@ function App({ test1 }) {
     console.log(formData)
     upload(formData)
   }
-  // useEffect(() => {
+  //  useEffect(() => {
   //   test1(resultState)
   // }, [resultState])
 
