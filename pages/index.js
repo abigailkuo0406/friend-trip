e9edb57e39e5552ce9b404b583a678ccc3752e6c
abigailkuo <abigailkuo0406@gmail.com>
2023-08-06 15:43:07 +0800

    首頁(缺輪播牆)

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,8 @@ import Btn from '@/components/common/button/btn-normal'
 export default function AdminIndex() {
   return (
     <>
-      <article className="blog-post">
-        {/* <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p> */}
-        <div className={`${HomePageStyle.card} ${HomePageStyle.cardPair}`}>
+      <article className="blog-post mb-4">
+        <div className={`${HomePageStyle.cardLong} ${HomePageStyle.cardPair}`}>
           <p className={HomePageStyle.cardTitle}>認識理想中的他與她！</p>
           <Btn
             btnText='抽好友這裡去→'
@@ -23,78 +14,31 @@ export default function AdminIndex() {
           />
         </div>
       </article>
-      <article className="blog-post">
-      <div className={HomePageStyle.card}>
-          <p className={HomePageStyle.cardTitle}>認識理想中的他與她！</p>
-          <Btn
-            btnText='抽好友這裡去→'
-            addClassforButton='btn-light'
-          />
-        </div>
-        {/* <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p> */}
+      <article className="blog-post d-flex mb-3">
+        <a href='/custom-itinerary/create-task' className={`${HomePageStyle.cardItinerary} ${HomePageStyle.cardMain} me-3`}>
+          <p className={HomePageStyle.cardText}>建立專屬行程</p>
+        </a>
+
+
+        <a href='/restaurant' className={`${HomePageStyle.cardRestaurant} ${HomePageStyle.cardMain}`}>
+          <p className={HomePageStyle.cardText}>浪漫餐廳首選</p>
+        </a>
       </article>
-      <article className="blog-post">
-        <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p>
+
+      <article className="blog-post d-flex mb-4">
+        <a href='/official-itinerary' className={`${HomePageStyle.cardOfficial} ${HomePageStyle.cardMain} me-3`}>
+          <p className={HomePageStyle.cardText}>官方行程推薦</p>
+        </a>
+        <a href='/product' className={`${HomePageStyle.cardProduct} ${HomePageStyle.cardMain}`}>
+          <p className={HomePageStyle.cardText}>心動時分小物</p>
+        </a>
       </article>
-      <article className="blog-post">
-        <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p>
-      </article>
-      <article className="blog-post">
-        <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p>
-      </article>
-      <article className="blog-post">
-        <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p>
-      </article>
-      <article className="blog-post">
-        <h2 className="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <hr />
-        <p>
-          This is some additional paragraph placeholder content. It has been
-          written to fill the available space and show how a longer snippet of
-          text affects the surrounding content. We'll repeat it often to keep
-          the demonstration flowing, so be on the lookout for this exact same
-          string of text.
-        </p>
+
+      <article className="blog-post mb-4">
+        <a href='/forum' className={`${HomePageStyle.cardLong} ${HomePageStyle.cardForum}`}>
+          <p className={HomePageStyle.cardText}>論壇</p>
+
+        </a>
       </article>
     </>
   )
