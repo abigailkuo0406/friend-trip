34d21d117f5175798326a024d932e048b07c1c61
Thomas <niutingyuan@gmail.com>
2023-08-08 11:15:48 -0800

    增加判斷登入與否，如果沒有登入，則不顯示我的發文

--- components/forum-comps/NavBar.jsx
+++ components/forum-comps/NavBar.jsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
 import styles from '@/components/forum-comps/NavBar.module.css'
+import AuthContext from '@/context/AuthContext'
+import { useContext } from 'react'
 export default function NavBar() {
+  const { auth } = useContext(AuthContext)
+  // 👇 判斷是否登入，兩個驚歎號作用為強制轉換數字為布林值
+  const isLoggedIn = !!auth.member_id
+  // ☝️ 判斷是否登入
   return (
     <nav className={`d-flex flex-row justify-content-between`}>
       <div>
@@ -13,11 +19,13 @@ export default function NavBar() {
           <h3>新增文章</h3>
         </Link>
       </div>
-      <div>
-        <Link href="/forum/my-posts" className={styles.navlink}>
-          <h3>我發的文</h3>
-        </Link>
-      </div>
+      {isLoggedIn && (
+        <div>
+          <Link href="/forum/my-posts" className={styles.navlink}>
+            <h3>我發的文</h3>
+          </Link>
+        </div>
+      )}
     </nav>
   )
 }
