f49b00d90d8a98f3c3fb671d57cca2411f4feda4
abigailkuo <abigailkuo0406@gmail.com>
2023-08-09 05:08:33 +0800

    新增評論前後端串接完成

--- components/reserve/comment.js
+++ components/reserve/comment.js
@@ -9,16 +9,35 @@ import styles from '@/components/reserve/reserve.module.css'
 
 export default function Comment({
     reservationId,
-    restId,
+    restId = 0,
     restName,
     restImg
 }) {
     const { auth } = useContext(AuthContext)
-    console.log('評論元件的auth', auth)
+    // console.log('評論元件的auth', auth)
 
 
     const [textAreaValue, setTextAreaValue] = useState()
     const [textAreaName, setTextAreaName] = useState()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3002/comment/addcomment', {
+            method: "POST",
+            body: JSON.stringify({
+                RestID: restId,
+                member_id: auth.member_id,
+                RestStarP: 5,
+                ComtText: textAreaValue,
+
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+    }
+
 
     return (
         <>
@@ -34,50 +53,53 @@ export default function Comment({
                                 type="button"
                                 class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
-                            <h1>{restName}</h1>
-                            <p>訂單編號{reservationId}</p>
-                            <p>餐廳地址</p>
-                            <div className='d-flex  align-items-center  '>
-                                <Image
-                                    src={`http://localhost:3002/face/${auth.images}`}
-                                    className={styles.avatar}
-                                    alt='大頭貼'
-                                    width={50}
-                                    height={50} />
-                                <p>{auth.member_name}</p>
+                        <form id='comment' onSubmit={handleSubmit}>
+
+                            <div class="modal-body">
+
+                                <h1>{restName}</h1>
+                                <p>訂單編號{reservationId}</p>
+                                <p>餐廳地址</p>
+                                <div className='d-flex  align-items-center  '>
+                                    <Image
+                                        src={`http://localhost:3002/face/${auth.images}`}
+                                        className={styles.avatar}
+                                        alt='大頭貼'
+                                        width={50}
+                                        height={50} />
+                                    <p>{auth.member_name}</p>
+                                </div>
+                                <p>請給予餐廳評分</p>
+                                <TextArea
+                                    id=''
+                                    name=''
+                                    label='請寫下您的評論'
+                                    getValue={setTextAreaValue}
+                                    getName={setTextAreaName}
+                                    required='true'
+
+                                />
                             </div>
+                            <div class="modal-footer">
+                                <Btn
+                                    btnText='取消評論'
+                                    addClassforButton="btn-light"
 
-                            <p>請給予餐廳評分</p>
-                            <TextArea
-                                id=''
-                                name=''
-                                label='請寫下您的評論'
-                                getValue={setTextAreaValue}
-                                getName={setTextAreaName}
-                                required='true'
-
-                            />
-
-                        </div>
-                        <div class="modal-footer">
-                            <Btn
-                                btnText='取消評論'
-                                addClassforButton="btn-light"
-
-                            />
-                            <Btn
-                                btnText='發布評論'
-                                type="submit"
-                                value="submit"
-                                addClassforButton="btn-dark ms-3" //.btn-dark：深色按鈕 .btn-light：淺色按鈕 .btn-white：白色按鈕
-                                disabled={false} // fase：可點，true：不可點
-                                bsModl3={`modal`}
-                            />
-                            {/* <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                />
+                                <Btn
+                                    btnText='發布評論'
+                                    type="submit"
+                                    value="submit"
+                                    addClassforButton="btn-dark ms-3" //.btn-dark：深色按鈕 .btn-light：淺色按鈕 .btn-white：白色按鈕
+                                    disabled={false} // fase：可點，true：不可點
+                                    bsModl3={`modal`}
+                                />
+                                {/* <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="button" class="btn btn-primary">Save changes</button> */}
-                        </div>
+                            </div>
+                        </form>
                     </div>
+
                 </div>
             </div>
 
