cb939a060f243cd5144eefd4d14262c671633d74
Thomas <niutingyuan@gmail.com>
2023-08-10 17:22:04 -0800

    新增沒有貼文圖片

--- components/forum-comps/EmptyStateMessage.jsx
+++ components/forum-comps/EmptyStateMessage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import noPost from '@/public/img/forum-img/noPostAdd.jpg'
+import Image from 'next/image'
 
 function EmptyStateMessage() {
   return (
-    <h1>您尚未發表</h1>
+    <>
+      <Image className='img-fluid' src={noPost} alt="No-Post-Empty" />
+    </>
   )
 }
 
