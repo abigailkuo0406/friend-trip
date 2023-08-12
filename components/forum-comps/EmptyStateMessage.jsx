26f682783d1d6247c8693c51dde7e0433e56cd4c
Thomas <niutingyuan@gmail.com>
2023-08-12 10:04:36 +0800

    修改尚未發表文章的顯示

--- components/forum-comps/EmptyStateMessage.jsx
+++ components/forum-comps/EmptyStateMessage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import noPost from '@/public/img/forum-img/noPostAdd.jpg'
 import Image from 'next/image'
+import styles from './emptyMsg.module.css'
 
 function EmptyStateMessage() {
   return (
-    <>
-      <Image className='img-fluid' src={noPost} alt="No-Post-Empty" />
-    </>
+    <div className={styles.container}>
+      <h1>您尚未發表文章</h1>
+      <h1>請新增文章</h1>
+      {/* <Image className="img-fluid" src={noPost} alt="No-Post-Empty" /> */}
+    </div>
   )
 }
 
