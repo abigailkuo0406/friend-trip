2f75714de33ecb2376a64c69c6add922c5fb6e68
Thomas <niutingyuan@gmail.com>
2023-08-12 12:54:42 +0800

    更改沒有文章的輸出文字

--- components/forum-comps/EmptyStateMessage.jsx
+++ components/forum-comps/EmptyStateMessage.jsx
@@ -6,9 +6,7 @@ import styles from './emptyMsg.module.css'
 function EmptyStateMessage() {
   return (
     <div className={styles.container}>
-      <h1>您尚未發表文章</h1>
-      <h1>請新增文章</h1>
-      {/* <Image className="img-fluid" src={noPost} alt="No-Post-Empty" /> */}
+      <h1>您尚未發表，請新增文章</h1>
     </div>
   )
 }
