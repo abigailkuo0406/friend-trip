97f64ceff4a7330c17b6fa4f7d02773a6e2921b4
John <japanwork6@gmail.com>
2023-08-14 23:29:25 +0800

    0814 11:29

--- components/pairs/pairs-button2.js
+++ components/pairs/pairs-button2.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import BtnNormal from '@/components/common/button/btn-normal'
+import { useRouter } from 'next/router'
 export default function PairBtns2({ setPage, setImgIndex, imgIndex, page }) {
+  const router = useRouter()
+  const change = () => {
+    router.push('./pair')
+  }
   return (
     <>
       <div className="d-flex justify-content-center gap-5 mt-3">
-        {/* <BtnNormal btnText="開始聊天" addClassforButton="btn-light" /> */}
+        <BtnNormal
+          btnText="改變命運"
+          addClassforButton="btn-light"
+          onClick={change}
+        />
 
         <BtnNormal
           btnText="尋找下一位"
